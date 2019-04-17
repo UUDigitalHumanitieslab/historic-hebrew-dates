d11060364794dfad0196fcb0dd746e7cd249c675
Sheean Spoel <s.j.j.spoel@uu.nl>
2019-04-17 15:28:04 +0200

    Initial number parser

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='historical-hebrew-dates',
+    python_requires='>=3.6, <4',
+    version='0.0.1',
+    description='Extracting historical Hebrew dates from text.',
+    long_description=long_description,
+    author='Digital Humanities Lab, Utrecht University',
+    author_email='digitalhumanities@uu.nl',
+    url='https://github.com/UUDigitalHumanitieslab/historic-hebrew-dates',
+    license='MIT',
+    packages=['historic-hebrew-dates'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'historic-hebrew-dates = historic-hebrew-dates.__main__:main'
+        ]
+    })
